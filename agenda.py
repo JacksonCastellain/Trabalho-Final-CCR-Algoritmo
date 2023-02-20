0d7cd45f88682c64e8401894bea50fc782d118ec
Jackson Castellain <jgcastellain@gmail.com>
2023-02-19 23:14:23 -0300

    Inserir com verificação de mesma data e hora

--- agenda.py
+++ agenda.py
@@ -5,108 +5,113 @@ class Agenda():
   descricao = None
 
 
-def inserir(e):
-  e.data = input('\nData: ')
-  e.hora = input('Hora: ')
-  e.duracao = input('Duração: ')
-  e.descricao = input('Descrição: ')
+def listaVazia(eventos):
+  if len(eventos)==0:
+    print('\n\t A agenda está vazia!')
+    return True
+  return False
+
+
+def inserir(eventos,data,hora,duracao,descricao,e):
+  e.data = data
+  e.hora = hora
+  e.duracao = duracao
+  e.descricao = descricao
+  for i in range(len(eventos)):
+    if(eventos[i].data == data and eventos[i].hora == hora):
+      print('\nJá existe um compromisso agendado para a mesma data e horário.')
+      del e.data
+      del e.hora
+      del e.duracao
+      del e.descricao
   return e
 
 
 def listar(eventos):
   if len(eventos) == 0:
-      print('\n\t  Agenda vazia.')
+      print('\n\t  A agenda está vazia!')
   for i in range(len(eventos)):
+    if eventos[i].data == None:
+      del eventos[i]
+    else:
       print(
           '\nData: ', eventos[i].data, 
           '\nHora: ', eventos[i].hora, 
           '\nDuração: ', eventos[i].duracao, 
           '\nDescrição: ',eventos[i].descricao 
           )
+      print('#'*70)
+
+
+def listarData(eventos,data):
+  flag = False
+  for i in range(len(eventos)):
+    if(eventos[i].data == data):
+      print(
+          '\nData: ', eventos[i].data, 
+          '\nHora: ', eventos[i].hora, 
+          '\nDuração: ', eventos[i].duracao, 
+          '\nDescrição: ',eventos[i].descricao 
+          )
+      print('#'*70)
+      flag = True
+  if flag == False:
+    print('\nAgenda Vazia')
+ 
+
+def listarHora(eventos,data,hora):
+  flag = False
+  for i in range(len(eventos)):
+    if(eventos[i].data == data and eventos[i].hora == hora):
+      print(
+          '\nHora: ', eventos[i].hora, 
+          '\nDuração: ', eventos[i].duracao, 
+          '\nDescrição: ',eventos[i].descricao 
+          )
       print('#'*70) 
+      flag = True
+  if flag == False:
+    print('\nAgenda Vazia')
 
 
-def listardata(eventos,data):
-  if len(eventos) == 0:
-    print('\n\tA agenda está vazia!')
-    flag = False
-    for i in range(len(eventos)):
-      if(eventos[i].data == data):
-        print(
-            '\nData: ', eventos[i].data, 
-            '\nHora: ', eventos[i].hora, 
-            '\nDuração: ', eventos[i].duracao, 
-            '\nDescrição: ',eventos[i].descricao 
-            )
-        print('#'*70)
-        flag = True
-    if flag == False:
-      print('\nAgenda Vazia')
-  else:
-    print('\nA agenda está vazia!')
+def editarEvento(eventos,data,hora):
+  flag = False
+  for i in range(len(eventos)):
+    if(eventos[i].data == data and eventos[i].hora == hora):
+      print(
+          '\nDuração: ', eventos[i].duracao, 
+          '\nDescrição: ',eventos[i].descricao 
+          )
+      print('#'*70)
+      print()
+      eventos[i].duracao, = input('Nova duração: ')
+      eventos[i].descricao  = input('Nova descrição: ')
+      flag = True
+  if flag == False:
+    print('\nCompromisso não encontrado')
 
 
-def listar_hora(eventos,data,hora):
-  if len(eventos) != 0:
-    flag = False
-    for i in range(len(eventos)):
-      if(eventos[i].data == data and eventos[i].hora == hora):
-        print(
-            '\nHora: ', eventos[i].hora, 
-            '\nDuração: ', eventos[i].duracao, 
-            '\nDescrição: ',eventos[i].descricao 
-            )
-        print('#'*70) 
-        flag = True
-    if flag == False:
-      print('\nAgenda Vazia')
-  else:
-    print('\nA agenda está vazia!') 
-
-
-def editar_evento(eventos,data,hora):
-  if len(eventos) != 0:
+def deletarData(eventos, data,hora):
     flag = False
     for i in range(len(eventos)):
       if(eventos[i].data == data and eventos[i].hora == hora):
-        print(
-            '\nDuração: ', eventos[i].duracao, 
-            '\nDescrição: ',eventos[i].descricao 
-            )
-        print('#'*70)
-        print()
-        eventos[i].duracao, = input('Nova duração: ')
-        eventos[i].descricao  = input('Nova descrição: ')
+        del eventos[i]
+        print("\n Evento removido!")
         flag = True
     if flag == False:
-      print('\nAgenda Vazia')
-  else:
-    print('\nA agenda está vazia!')
+      print('\n Compromisso não encontrado')
 
 
 def deletarTudo(eventos):
   if len(eventos) != 0:
     eventos.clear()
-    print('\nTodos os contatos foram removidos!')
+    print('\nTodos os eventos foram removidos!')
   else:
-    print('\nA agenda está vazia!')
-
-
-def deletardata(eventos, data):
-  if len(eventos) != 0:
-    flag = False
-    for i in range(len(eventos)):
-      if(eventos[i].data==data):
-        eventos.pop(i)
-        print("\nContato removido!")
-        flag = True
-    if flag == False:
-      print('\nAgenda Vazia Nesta Data')
-  else:
-    print('\nA agenda está vazia!')
+    print('\n\t A agenda está vazia!')
 
 
 eventos = []
+
 
 while True:
   print('\n1 - Inserir')
@@ -118,31 +123,50 @@ while True:
   print('7 - Deletar todos os eventos')
   print('8 - Sair.')
 
-
   op = int(input('Entre com a opção desejada: '))
 
   if op == 1:
+      data = input('Digite a data: ')
+      hora = input('Digite o hora: ')
+      duracao = input('Duração: ')
+      descricao = input('Descrição: ')
       e = Agenda()
-      eventos.append(inserir(e))
+      eventos.append(inserir(eventos,data,hora,duracao,descricao,e))
   elif op == 2:
     listar(eventos)
   elif op == 3:
-    data = input('Digite a data:')
-    listardata(eventos,data)
+    if listaVazia(eventos) == True:
+      continue
+    else:
+      data = input('Digite a data:')
+      listarData(eventos,data)
   elif op == 4:
-    data = input('Digite a data: ')
-    hora = input('Digite o hora: ')
-    listar_hora(eventos,data,hora)
+    if listaVazia(eventos) == True:
+      continue
+    else:
+      data = input('Digite a data: ')
+      hora = input('Digite o hora: ')
+      listarHora(eventos,data,hora)
   elif op == 5:
-    data = input('Digite a data: ')
-    hora = input('Digite o hora: ')
-    editar_evento(eventos,data,hora)
+    if listaVazia(eventos) == True:
+      continue
+    else:
+      data = input('Digite a data: ')
+      hora = input('Digite o hora: ')
+      editarEvento(eventos,data,hora)
   elif op == 6:
-    data = input('Digite a data:')
-    deletardata(eventos,data)
+    if listaVazia(eventos) == True:
+      continue
+    else:
+      data = input('Digite a data:')
+      hora = input('Digite o hora: ')
+      deletarData(eventos,data,hora)
   elif op == 7:
     deletarTudo(eventos)
   elif op == 8:
     break
   else:
     print('\nDigite um número dentro das opções!')
+
+
+     
